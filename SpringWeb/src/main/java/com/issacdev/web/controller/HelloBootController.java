19342fe5fe75fcbea961f1a93893763998f58781
issaclau <issaclau@laumingtaks-MacBook-Pro.local>
2016-07-02 18:21:42 +0800

    add modulemap to boot controller

--- SpringWeb/src/main/java/com/issacdev/web/controller/HelloBootController.java
+++ SpringWeb/src/main/java/com/issacdev/web/controller/HelloBootController.java
@@ -2,7 +2,9 @@ package com.issacdev.web.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,12 @@ public class HelloBootController {
         return "Greetings from Spring Boot!";
     }
     
+	@RequestMapping(value="/hello", method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		model.addAttribute("message", "Hello Spring MVC Framework!");
+		return "hello";
+	}
+	
     public static void main(String[] args) throws Exception {
         SpringApplication.run(HelloBootController.class, args);
     }
